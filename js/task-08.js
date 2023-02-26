8082b72e200c7bf6970e1687763244448b6e4840
TarasDer <tderev.switch@gmail.com>
2023-02-26 22:26:28 +0200

    завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-const formEl = document.querySelector("login-form");
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Заповніть всі поля");
+  }
+}
